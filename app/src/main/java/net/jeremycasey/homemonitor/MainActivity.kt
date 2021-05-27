db0e5499126d3eeff791622dd3abb3e45b7b7395
Jeremy Casey <jeremy8883@gmail.com>
2021-05-27 22:46:08 +1000

    Quick try at using jetpack compose

--- app/src/main/java/net/jeremycasey/homemonitor/MainActivity.kt
+++ app/src/main/java/net/jeremycasey/homemonitor/MainActivity.kt
@@ -3,11 +3,16 @@ package net.jeremycasey.homemonitor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.jeremycasey.homemonitor.ui.theme.HomeMonitorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +22,7 @@ class MainActivity : ComponentActivity() {
       HomeMonitorTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
+          WeatherWidget()
         }
       }
     }
@@ -25,14 +30,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun WeatherWidget() {
+  WeatherWidgetView(current = 19.5, min = 10, max = 20)
+}
+
+@Composable
+fun WeatherWidgetView(current: Number, min: Number, max: Number) {
+  Column (modifier = Modifier.padding(16.dp)) {
+    Row {
+      Text(text = "Current: ")
+      Text(text = "$current")
+    }
+    Row {
+      Text(text = "Min: ")
+      Text(text = "$min")
+    }
+    Row {
+      Text(text = "Max: ")
+      Text(text = "$max")
+    }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
   HomeMonitorTheme {
-    Greeting("Android")
+    WeatherWidgetView(current = 19.5, min = 10, max = 20)
   }
 }
