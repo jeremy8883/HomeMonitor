fdb3a0eb12204b275fce860fa922d2d66291cd33
Jeremy Casey <jeremy8883@gmail.com>
2021-06-04 11:13:10 +1000

    Make app full screen

--- app/src/main/java/net/jeremycasey/homemonitor/MainActivity.kt
+++ app/src/main/java/net/jeremycasey/homemonitor/MainActivity.kt
@@ -1,6 +1,7 @@
 package net.jeremycasey.homemonitor
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -55,6 +56,28 @@ class MainActivity : ComponentActivity() {
       }
     }
   }
+
+  // Code taken from: https://developer.android.com/training/system-ui/immersive
+  override fun onWindowFocusChanged(hasFocus: Boolean) {
+    super.onWindowFocusChanged(hasFocus)
+    if (hasFocus) hideSystemUI()
+  }
+
+  private fun hideSystemUI() {
+    // Enables regular immersive mode.
+    // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+    // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+        // Set the content to appear under the system bars so that the
+        // content doesn't resize when the system bars hide and show.
+        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        // Hide the nav bar and status bar
+        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+  }
+
 
 //  fun timedOpenAppBackUp() {
 //    val futureDate = DateTime.now().plusMillis(appCloseTimeoutMs)
