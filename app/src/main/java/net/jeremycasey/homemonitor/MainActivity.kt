7d2a99610b7ff818320ad7a23b0e2b6ae836d242
Jeremy Casey <jeremy8883@gmail.com>
2021-06-04 23:07:14 +1000

    Move function

--- app/src/main/java/net/jeremycasey/homemonitor/MainActivity.kt
+++ app/src/main/java/net/jeremycasey/homemonitor/MainActivity.kt
@@ -1,7 +1,6 @@
 package net.jeremycasey.homemonitor
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -12,6 +11,7 @@ import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import net.jeremycasey.homemonitor.ui.theme.HomeMonitorTheme
+import net.jeremycasey.homemonitor.utils.hideSystemUi
 import net.jeremycasey.homemonitor.widgets.calendar.CalendarWidget
 import net.jeremycasey.homemonitor.widgets.calendar.CalendarWidgetViewModel
 import net.jeremycasey.homemonitor.widgets.calendar.CalendarWidgetViewModelFactory
@@ -67,24 +67,8 @@ class MainActivity : ComponentActivity() {
   // Code taken from: https://developer.android.com/training/system-ui/immersive
   override fun onWindowFocusChanged(hasFocus: Boolean) {
     super.onWindowFocusChanged(hasFocus)
-    if (hasFocus) hideSystemUI()
+    if (hasFocus) hideSystemUi(window)
   }
-
-  private fun hideSystemUI() {
-    // Enables regular immersive mode.
-    // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-    // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-    window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-        // Set the content to appear under the system bars so that the
-        // content doesn't resize when the system bars hide and show.
-        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        // Hide the nav bar and status bar
-        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        or View.SYSTEM_UI_FLAG_FULLSCREEN)
-  }
-
 
 //  fun timedOpenAppBackUp() {
 //    val futureDate = DateTime.now().plusMillis(appCloseTimeoutMs)
