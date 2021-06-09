515fc02f63213097845344436cccf65e6dd0e076
Jeremy Casey <jeremy8883@gmail.com>
2021-06-10 00:11:26 +1000

    Add empty state

--- app/src/main/java/net/jeremycasey/homemonitor/widgets/messageBoard/MessageBoardWidget.kt
+++ app/src/main/java/net/jeremycasey/homemonitor/widgets/messageBoard/MessageBoardWidget.kt
@@ -7,14 +7,20 @@ import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
@@ -90,18 +96,25 @@ fun MessageBoardWidget(viewModel: MessageBoardWidgetViewModel) {
 @Composable
 fun MessageBoardWidgetView(postedItems: List<MessageItem>, onClearItem: (postedItem: MessageItem) -> Unit) {
   WidgetCard(scrollable = Scrollable.horizontal) {
-    Row(
-      horizontalArrangement = Arrangement.Center,
-      verticalAlignment = Alignment.CenterVertically,
-      modifier = Modifier.fillMaxWidth()
-    ) {
-      postedItems.forEach { item ->
-        Box(Modifier.padding(10.dp, 0.dp).clickable { onClearItem(item) }) {
-          Image(
-            painter = BitmapPainter(item.image.asImageBitmap()),
-            contentDescription = item.message,
-            modifier = Modifier.width(150.dp).height(150.dp)
-          )
+    if (postedItems.isEmpty()) {
+      Text(
+        text ="There are no messages",
+        style = TextStyle(textAlign = TextAlign.Center, color = Color.Gray, fontStyle = FontStyle.Italic)
+      )
+    } else {
+      Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+      ) {
+        postedItems.forEach { item ->
+          Box(Modifier.padding(10.dp, 0.dp).clickable { onClearItem(item) }) {
+            Image(
+              painter = BitmapPainter(item.image.asImageBitmap()),
+              contentDescription = item.message,
+              modifier = Modifier.width(150.dp).height(150.dp)
+            )
+          }
         }
       }
     }
